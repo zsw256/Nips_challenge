5709a1b3d91a5048989b91970b4a3aaeac51f5f0
Ubuntu <nips@myVm.llqxh5xbm4buhn5nlthm4mjnng.bx.internal.cloudapp.net>
2023-10-26 06:20:47 +0000

    change traning pramaters

--- train.sh
+++ train.sh
@@ -1,25 +1,30 @@
-deepspeed --include localhost:0 --master_port 9090 ./LLaMA-Factory/src/train_bash.py \
+WANDB_DISABLED=True deepspeed --include localhost:0 --master_port 9090 ./LLaMA-Factory/src/train_bash.py \
     --deepspeed ./configs/ds_config_zero2.json \
     --stage sft \
     --model_name_or_path Qwen/Qwen-14B \
     --use_fast_tokenizer True \
     --do_train \
-    --dataset_dir ./data/dataset \
+    --dataset_dir /home/nips/Nips_challenge/data/dataset \
     --dataset tulu_merge \
     --template default \
     --cutoff_len 2048 \
+    --neft_alpha 5 \
     --finetuning_type lora \
     --lora_rank 64 \
     --lora_target "c_attn","c_proj" \
-    --output_dir ./output/tulu_merge \
+    --output_dir ./output/qwen_merge \
     --overwrite_cache \
     --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --save_steps 2000 \
-    --learning_rate 5e-5 \
+    --save_steps 20000 \
+    --warmup_ratio 0.01 \
+    --learning_rate 3e-4 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
     --num_train_epochs 1.0 \
     --plot_loss \
-    --fp16 \
+    --bf16 \
     --overwrite_output_dir
